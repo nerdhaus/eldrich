f823fc153b96bf44652336ec84907f8d08b0e267
Jeff Eaton <eaton@lullabot.com>
2016-10-22 18:05:19 -0500

    Tweaky twiddle.

--- config/server/composer.sh
+++ config/server/composer.sh
@@ -6,6 +6,3 @@ echo 'export PATH="$HOME/.composer/vendor/bin:$PATH"' >> ~/.profile
 
 cd /vagrant web
 composer install
-
-ln -s /vagrant/modules /vagrant/drupal/web/modules/custom
-ln -s /vagrant/themes /vagrant/drupal/web/themes/custom
