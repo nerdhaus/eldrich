37a5d115e7f8537ca77fc59626933e8114bc6efd
Jeff Eaton <eaton@lullabot.com>
2016-10-22 19:34:05 -0500

    Fixing local bin

--- config/server/composer.sh
+++ config/server/composer.sh
@@ -2,10 +2,12 @@
 
 cd /vagrant
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
-echo 'export PATH="$HOME/.composer/vendor/bin:$PATH"' >> ~/.profile
+echo 'export PATH="/vagrant/vendor/bin:$PATH"' >> ~/.profile
+
 echo 'export DB_NAME="eldrich"' >> ~/.profile
 echo 'export DB_USER="root"' >> ~/.profile
 echo 'export DB_PASS="root"' >> ~/.profile
 
 cd /vagrant web
 composer install
+composer update
