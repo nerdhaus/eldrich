962bb9b7899e3f5284002344ca929e00e17e85f7
Jeff Eaton <eaton@lullabot.com>
2016-11-03 21:10:18 -0500

    Override the query to support multiple bundles.

--- web/modules/custom/ep_import/src/Plugin/migrate/process/EntityLookupWithValue.php
+++ web/modules/custom/ep_import/src/Plugin/migrate/process/EntityLookupWithValue.php
@@ -25,6 +25,9 @@ class EntityLookupWithValue extends EntityLookup {
    * {@inheritdoc}
    */
   public function transform($value, MigrateExecutableInterface $migrateExecutable, Row $row, $destinationProperty) {
+
+    drush_print_r($this->lookupBundle);
+
     // We want to handle these nicely later.
     $this->extraKey = 'override';
     $this->delimiter = ':';
@@ -39,15 +42,69 @@ class EntityLookupWithValue extends EntityLookup {
       $title = trim($value);
       $notes = NULL;
     }
-drush_print_r($title);
+
     if ($entity = parent::transform($title, $migrateExecutable, $row, $destinationProperty)) {
       $results = array(
         'target_id' => $entity,
         $this->extraKey => $notes,
       );
-      drush_print_r($results);
     }
     return $results;
+  }
+
+  /**
+   * Checks for the existence of some value.
+   *
+   * @param $value
+   * The value to query.
+   *
+   * @return mixed|null
+   *   Entity id if the queried entity exists. Otherwise NULL.
+   */
+  protected function query($value) {
+    // Entity queries typically are case-insensitive. Therefore, we need to
+    // handle case sensitive filtering as a post-query step. By default, it
+    // filters case insensitive. Change to true if that is not the desired
+    // outcome.
+    $ignoreCase = !empty($this->configuration['ignore_case']) ?: FALSE;
+
+    $multiple = is_array($value);
+
+    $query = $this->entityManager->getStorage($this->lookupEntityType)
+      ->getQuery()
+      ->condition($this->lookupValueKey, $value, $multiple ? 'IN' : NULL);
+
+    if ($this->lookupBundleKey) {
+      // This is the one line we're changing. If multiple bundles are specified,
+      // we can query them all. It's kind of dumb, but we need it so we can split
+      // Generic Gear and Weapons across multiple node types while still pulling
+      // them in with a single query.
+      $query->condition($this->lookupBundleKey, $this->lookupBundle, is_array($this->lookupBundle) ? 'IN' : NULL);
+    }
+    $results = $query->execute();
+
+    if (empty($results)) {
+      return NULL;
+    }
+
+    // By default do a case-sensitive comparison.
+    if (!$ignoreCase) {
+      // Returns the entity's identifier.
+      foreach ($results as $k => $identifier) {
+        $result_value = $this->entityManager->getStorage($this->lookupEntityType)->load($identifier)->{$this->lookupValueKey}->value;
+        if (($multiple && !in_array($result_value, $value, TRUE)) || (!$multiple && $result_value !== $value)) {
+          unset($results[$k]);
+        }
+      }
+    }
+
+    if ($multiple && !empty($this->destinationProperty)) {
+      array_walk($results, function (&$value) {
+        $value = [$this->destinationProperty => $value];
+      });
+    }
+
+    return $multiple ? array_values($results) : reset($results);
   }
 }
 
