38316cc1e3bc90bd09bc004829ea13c067e30c5e
Jeff Eaton <eaton@lullabot.com>
2017-01-08 17:45:44 -0600

    Major cleanup, w/handling of multiline fields

---
+++ web/modules/custom/ep_import/src/Plugin/migrate/process/LineBreaker.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Drupal\ep_import\Plugin\migrate\process;
+
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\Row;
+
+/**
+ * This plugin turns linebreaks into paragraph tags.
+ *
+ * @MigrateProcessPlugin(
+ *   id = "line_breaker"
+ * )
+ */
+class LineBreaker extends ProcessPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+
+    if (empty($value)) {
+      return $value;
+    }
+
+    // Strip out nasty linebreaks, a problem in our source data
+    $value = preg_replace("/\s?[\n\r]{2,10}/", "</p>\r<p>", $value);
+    $value = preg_replace("/\s?[\n\r]/", "", $value);
+
+    $value = "<p>" . $value . "</p>";
+
+    return $value;
+  }
+}
