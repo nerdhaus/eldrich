849ccd52543d0df77ee02279cc1643b5e3d9f0ad
Jeff Eaton <eaton@lullabot.com>
2017-01-14 18:26:45 -0600

    moving to separate charsheet template

---
+++ web/modules/custom/eldrich/src/Controller/CharacterViewController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Drupal\eldrich\Controller;
+
+use Drupal\Component\Utility\Xss;
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Datetime\DateFormatterInterface;
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Drupal\Core\Render\RendererInterface;
+use Drupal\Core\Url;
+use Drupal\node\NodeStorageInterface;
+use Drupal\node\NodeTypeInterface;
+use Drupal\node\NodeInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\node\Controller\NodeViewController;
+use Drupal\Core\Entity\EntityInterface;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+
+/**
+ * Returns responses for Eldrich-specific node route variations.
+ */
+class CharacterViewController extends NodeViewController {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function combatCard(EntityInterface $node) {
+    if (in_array($node->bundle(), ['pc', 'npc', 'creature', 'robot'])) {
+      return parent::view($node, 'card');
+    }
+    else {
+      throw new ResourceNotFoundException();
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function charSheet(EntityInterface $node) {
+    if (in_array($node->bundle(), ['pc', 'npc'])){
+      return parent::view($node, 'charsheet');
+    }
+    else {
+      throw new ResourceNotFoundException();
+    }
+  }
+}
