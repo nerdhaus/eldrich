9c224304071bbc566a3571f5e8636e7cd89ecb37
Jeff Eaton <eaton@lullabot.com>
2017-01-18 19:14:22 -0600

    Reindex search when flushing

--- flush.sh
+++ flush.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 drush sql-drop
 mysql -uroot -proot eldrich < /vagrant/initial.sql
 drush config-import -y
@@ -5,3 +6,4 @@ drush mi --group=eldrich_core
 drush mi --group=eldrich_gear
 drush mi --group=eldrich_chars
 drush mi --group=eldrich_content
+drush search-index
