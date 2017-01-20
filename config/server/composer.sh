bdcf34c102f66f331046ad0e5944763f6d21157a
Jeff Eaton <eaton@lullabot.com>
2017-01-19 22:48:55 -0600

    So much hacking. Mostly just reworked /stats

--- config/server/composer.sh
+++ config/server/composer.sh
@@ -3,6 +3,8 @@
 cd /vagrant
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
 echo 'export PATH="/vagrant/vendor/bin:$PATH"' >> ~/.profile
+echo 'export ELDRICH_MYSQL_USER="root"' >> ~/.profile
+echo 'export ELDRICH_MYSQL_PASS="root"' >> ~/.profile
 
 cd /vagrant
 composer install
