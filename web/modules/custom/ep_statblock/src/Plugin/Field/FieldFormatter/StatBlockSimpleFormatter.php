67cc13f86dd5d255caf57611fd94e6cfce69cbc5
Jeff Eaton <eaton@lullabot.com>
2017-01-25 20:03:06 -0600

    Tweaking the statblock formatter
    Now allows individual stat properties to be hidden, and can switch between label, key, and "raw" display styles.

--- web/modules/custom/ep_statblock/src/Plugin/Field/FieldFormatter/StatBlockSimpleFormatter.php
+++ web/modules/custom/ep_statblock/src/Plugin/Field/FieldFormatter/StatBlockSimpleFormatter.php
@@ -25,6 +25,8 @@ class StatBlockSimpleFormatter extends FormatterBase {
   public static function defaultSettings() {
     return array(
         'visible_properties' => NULL,
+        'labels' => 'keys',
+        'delimiter' => ', ',
       ) + parent::defaultSettings();
   }
 
@@ -33,14 +35,34 @@ class StatBlockSimpleFormatter extends FormatterBase {
    */
   public function settingsForm(array $form, FormStateInterface $form_state) {
     $elements = parent::settingsForm($form, $form_state);
+
+    $elements['labels'] = array(
+      '#type' => 'select',
+      '#options' => [
+        'none' => t('None'),
+        'keys' => t('Keys'),
+        'labels' => t('Labels'),
+      ],
+      '#title' => t('Label type'),
+      '#default_value' => $this->getSetting('labels'),
+      '#required' => TRUE,
+    );
+
+    $elements['delimiter'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Property delimiter'),
+      '#default_value' => $this->getSetting('delimiter'),
+    );
+
+
     $def = $this->fieldDefinition;
     $stg = $def->getFieldStorageDefinition();
     $class = $def->getItemDefinition()->getClass();
-
     $property_definitions = $class::propertyDefinitions($stg);
 
     $elements['visible_properties'] = array(
       '#type' => 'checkboxes',
+      '#multiple' => TRUE,
       '#options' => [],
       '#title' => t('Visible properties'),
       '#default_value' => $this->getSetting('visible_properties'),
@@ -64,8 +86,11 @@ class StatBlockSimpleFormatter extends FormatterBase {
       $summary[] = t('Show all properties');
     }
     else {
-      $summary[] = t('Show @props', array('@props' => join($properties, ', ')));
+      $summary[] = t('Show @props', array('@props' => join($properties, $this->getSetting('delimiter'))));
     }
+    $label_style = $this->getSetting('labels');
+    $summary[] = t('Label style: @label', array('@label' => $label_style));
+
     return $summary;
   }
 
@@ -77,19 +102,34 @@ class StatBlockSimpleFormatter extends FormatterBase {
     $elements = [];
     $visible_properties = $this->getSetting('visible_properties');
 
+    $def = $this->fieldDefinition;
+    $stg = $def->getFieldStorageDefinition();
+    $class = $def->getItemDefinition()->getClass();
+    $property_definitions = $class::propertyDefinitions($stg);
+
     foreach ($items as $delta => $item) {
       $values = $item->getValue();
       $output = [];
 
       foreach ($values as $key => $value) {
         if ((empty($visible_properties) || !empty($visible_properties[$key])) && !empty($value)) {
-          $output[] = $key . ': ' . $value;
+          switch ($this->getSetting('labels')) {
+            case 'none':
+              $output[] = $value;
+              break;
+            case 'keys':
+              $output[] = strtoupper($key) . ': ' . $value;
+              break;
+            case 'labels':
+              $output[] = $property_definitions[$key]->getLabel() . ': ' . $value;
+              break;
+          }
         }
       }
 
       $elements[$delta] = [
         '#type' => 'markup',
-        '#markup' => strtoupper(join(', ', $output)),
+        '#markup' => join($this->getSetting('delimiter'), $output),
       ];
     }
 
