c569ce4e0b0e37e22325269f7661da5076fad520
Jeff Eaton <eaton@lullabot.com>
2017-01-25 18:02:20 -0600

    Allow properties to be hidden

--- web/modules/custom/ep_statblock/src/Plugin/Field/FieldFormatter/StatBlockSimpleFormatter.php
+++ web/modules/custom/ep_statblock/src/Plugin/Field/FieldFormatter/StatBlockSimpleFormatter.php
@@ -17,20 +17,72 @@ use Drupal\Core\Form\FormStateInterface;
  *   }
  * )
  */
-class StatBlockSimpleFormatter extends FormatterBase  {
+class StatBlockSimpleFormatter extends FormatterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return array(
+        'visible_properties' => NULL,
+      ) + parent::defaultSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    $elements = parent::settingsForm($form, $form_state);
+    $def = $this->fieldDefinition;
+    $stg = $def->getFieldStorageDefinition();
+    $class = $def->getItemDefinition()->getClass();
+
+    $property_definitions = $class::propertyDefinitions($stg);
+
+    $elements['visible_properties'] = array(
+      '#type' => 'checkboxes',
+      '#options' => [],
+      '#title' => t('Visible properties'),
+      '#default_value' => $this->getSetting('visible_properties'),
+      '#required' => FALSE,
+    );
+
+    foreach ($property_definitions as $k => $d) {
+      $elements['visible_properties']['#options'][$k] = $d->getLabel();
+    }
+
+    return $elements;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+    $summary = parent::settingsSummary();
+    $properties = $this->getSetting('visible_properties');
+    if (empty($properties)) {
+      $summary[] = t('Show all properties');
+    }
+    else {
+      $summary[] = t('Show @props', array('@props' => join($properties, ', ')));
+    }
+    return $summary;
+  }
+
 
   /**
    * {@inheritdoc}
    */
   public function viewElements(FieldItemListInterface $items, $langcode) {
     $elements = [];
+    $visible_properties = $this->getSetting('visible_properties');
 
     foreach ($items as $delta => $item) {
       $values = $item->getValue();
       $output = [];
 
       foreach ($values as $key => $value) {
-        if (!empty($value) && ($key != 'synthetic')) {
+        if (!empty($visible_properties[$key]) && !empty($value)) {
           $output[] = $key . ': ' . $value;
         }
       }
@@ -43,5 +95,4 @@ class StatBlockSimpleFormatter extends FormatterBase  {
 
     return $elements;
   }
-
 }
