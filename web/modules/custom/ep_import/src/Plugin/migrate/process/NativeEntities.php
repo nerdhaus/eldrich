4bee263092aff40f6336cb4375fb10f107ec0b39
Jeff Eaton <eaton@lullabot.com>
2017-02-13 09:30:53 -0600

    Imports for native attacks and abilities

---
+++ web/modules/custom/ep_import/src/Plugin/migrate/process/NativeEntities.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Drupal\ep_import\Plugin\migrate\process;
+
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\Row;
+use Drupal\migrate\Plugin\MigrationInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Entity\EntityManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * This plugin just grabs campaign ids.
+ *
+ * @MigrateProcessPlugin(
+ *   id = "native_entities",
+ *   handle_multiples = FALSE
+ * )
+ */
+class NativeEntities extends ProcessPluginBase implements ContainerFactoryPluginInterface {
+
+  /** @var \Drupal\Core\Entity\EntityManagerInterface */
+  protected $entityManager;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $pluginId, $pluginDefinition, EntityManagerInterface $entityManager) {
+    parent::__construct($configuration, $pluginId, $pluginDefinition);
+    $this->entityManager = $entityManager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $pluginId, $pluginDefinition, MigrationInterface $migration = NULL) {
+    return new static(
+      $configuration,
+      $pluginId,
+      $pluginDefinition,
+      $container->get('entity.manager')
+    );
+  }
+
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+    $entity_type = $this->configuration['type'];
+    $query = $this->entityManager->getStorage('component')->getQuery();
+    $ids = $query
+      ->condition('type', $entity_type)
+      ->condition('field_creature', trim($value))
+      ->execute();
+
+    $results = [];
+    foreach ($ids as $id) {
+      $results[]['target_id'] = $id;
+    }
+    return $results;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function multiple() {
+    return FALSE;
+  }
+}
