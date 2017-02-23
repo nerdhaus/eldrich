af50710015ada3427e3127c777d83ea2fe9b57d0
Jeff Eaton <eaton@lullabot.com>
2017-02-22 21:40:45 -0600

    Site aliases and SSH goodness

---
+++ drush/site-aliases/aliases.drushrc.php
@@ -0,0 +1,12 @@
+<?php
+$aliases['live'] = array(
+  'remote-host' => 'eldrich.host',
+  'remote-user' => 'eaton',
+  'root' => '/home/eaton/eldrich/web',
+  'uri' => 'http://eldrich.host',
+);
+
+$aliases["local"] = array (
+  'root' => '/vagrant/web',
+  'uri' => 'http://eldrich.local',
+);
