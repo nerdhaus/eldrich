55d4302101f1bfc46b4a949aa165a3b800dd4713
Jeff Eaton <eaton@lullabot.com>
2017-03-02 18:12:56 -0600

    Shifting date and anonymizer code to eldrich

--- web/themes/custom/veil/src/Plugin/Preprocess/ICComment.php
+++ web/themes/custom/veil/src/Plugin/Preprocess/ICComment.php
@@ -35,13 +35,12 @@ class ICComment extends PreprocessBase implements PreprocessInterface {
 
     $variables->identity = $this->makeNetworkId($character, $comment, $obscure);
     $variables->reputation = $this->getCharacterRep($character);
-    $variables->timestamp = $this->makeAFTime($comment->getCreatedTime());
+    $variables->timestamp = eldrich_af_time($comment->getCreatedTime());
   }
 
   private function makeNetworkId(NodeInterface $character, Comment $comment, $obscure = FALSE) {
     if ($obscure) {
-      $unique = (string) $character->id() . (string) $comment->id();
-      $identity = $this->hashToCallsign(hash('md2', $unique));
+      $identity = make_obscured_name($character->id(), $comment->id());
     }
     else {
       $identity = Html::cleanCssIdentifier(strtolower($character->label()));
@@ -68,84 +67,5 @@ class ICComment extends PreprocessBase implements PreprocessInterface {
     else {
       return '';
     }
-  }
-
-  private function makeAFTime($timeStamp = 0) {
-    // Jul 4 2005, the UTC Time of Deep Impact's contact with its target
-    // https://www.nasa.gov/mission_pages/deepimpact/timeline/index.html
-
-    $fall = 1120455926;
-    $time = $timeStamp - $fall;
-
-    $af[] = $time/31556926 % 12;  // to get year
-    $af[] = $time / 604800 % 52;  // to get weeks
-    $af[] = $time / 3600 % 24;    // to get hours
-    $af[] = $time / 60 % 60;      // to get minutes
-    // $af[] = $time % 60;        // to get seconds
-
-    return 'AF' . join('.', $af);
-  }
-
-  function hashToCallsign($hash) {
-    preg_match("/\D\d\d/", $hash, $output_array);
-    $str = str_split(array_pop($output_array));
-
-    $nato_alpha = [
-      'a' => 'alfa',
-      'b' => 'bravo',
-      'c' => 'charlie',
-      'd' => 'delta',
-      'e' => 'echo',
-      'f' => 'foxtrot',
-      'g' => 'golf',
-      'h' => 'hotel',
-      'i' => 'india',
-      'j' => 'juliett',
-      'k' => 'kilo',
-      'l' => 'lima',
-      'm' => 'mike',
-      'n' => 'november',
-      'o' => 'oscar',
-      'p' => 'papa',
-      'q' => 'quebec',
-      'r' => 'romeo',
-      's' => 'sierra',
-      't' => 'tango',
-      'u' => 'uniform',
-      'v' => 'victor',
-      'w' => 'whiskey',
-      'y' => 'yankee',
-      'z' => 'zulu'
-    ];
-    $nato_number = [
-      '0' => 'zero',
-      '1' => 'one',
-      '2' => 'two',
-      '3' => 'three',
-      '4' => 'four',
-      '5' => 'five',
-      '6' => 'six',
-      '7' => 'seven',
-      '8' => 'eight',
-      '9' => 'niner'
-    ];
-    $color_number = [
-      '0' => 'black',
-      '1' => 'blue',
-      '2' => 'brown',
-      '3' => 'green',
-      '4' => 'indigo',
-      '5' => 'jade',
-      '6' => 'orange',
-      '7' => 'pink',
-      '8' => 'purple',
-      '9' => 'violet'
-    ];
-
-    $str[0] = $nato_alpha[$str[0]];
-    $str[1] = $nato_number[$str[1]];
-    $str[2] = $color_number[$str[2]];
-
-    return join('-', $str);
   }
 }
